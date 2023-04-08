6cc8883aba3e079427c2d1c5589f2de8f150fc55
Alejandro Camas <alejandromcamas@gmail.com>
2023-04-08 17:22:25 -0600

    coneccion con movil y con desktop

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+/*esta parte es para la vercion de movil */
+const menoHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktoopMenu);
+/*aqui es para la vercion de movil */
+menoHamIcon.addEventListener('click', toggleMobilMenu);
 
 
 /**en esta funcion mandamos a llamar lo que nosotros colocamos en css lo cual es que uestre o no muestre el panenl */
@@ -10,3 +15,28 @@ function toggleDesktoopMenu() {
 }
 
 /**la palabra  o comando "toggle"  sirve para que cada que nosotros querramos que haya como un cambio de que aparezca o no aparezca*/
+
+/*aqui la vercion movil */
+function toggleMobilMenu () {
+    mobileMenu.classList.toggle('inactive');
+}
+
+
+/*const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
+}
+ por alguna razon esta igual per no funciona asi que tomare este*/
+
+ /*actualizacion, vi mi error era tenia una v en lugar de una b */
